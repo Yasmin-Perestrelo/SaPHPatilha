9b101931e0469fda231282b03018b7dd09696f2c
Yasmin Perestrelo <perestreloyasmin@gmail.com>
2022-09-29 12:14:27 -0300

    Criação do site SaPHPatilha

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+  <head>
+    <title>SaPHPatilha</title>
+  </head>
+  <Style>
+    nav ul {
+      display: flex;
+      list-style:none;
+      justify-content: space-evenly;
+      background-color:white;
+    } 
+
+    li{
+      margin-bottom: 10px;
+    }
+
+    body {
+      background-color: #2F3136;
+    } 
+
+    h1{
+      color:white;
+      font-size: 40px;
+      display: block;
+      margin: 0;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-right: -50%;
+      transform: translate(-50%, -50%);
+    }
+
+  </Style>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a style="text-decoration:none;" href="index.php">SaPHPatilha&nbsp; </a></li>
+          <li><a style="text-decoration:none;" href="produtos.php">Produtos&nbsp; </a></li>
+          <li><a style="text-decoration:none;" href="login.php">Login&nbsp; </a></li>
+          <li><a style="text-decoration:none;" href="cadastro.php">Cadastro</a></li>
+          <li><a style="text-decoration:none;" href="sobrenos.php">Sobre nós&nbsp; </a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <section>
+      <div>
+      <h1>Venha descobrir<br> o sapato perfeito<br> pra você!</h1>
+      </div>
+      </section>
+    </main>
+
+    <footer>
+  
+  </footer>
+    
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
---
+++ produtos.php
@@ -0,0 +1,144 @@
+<html>
+  <head>
+    <title>SaPHPatilha</title>
+  </head>
+  <Style>
+    nav ul{
+      display: flex;
+      list-style:none;
+      justify-content: space-evenly;
+    } 
+
+    div{
+      list-style:none;
+    } 
+
+    section {
+      display: flex;
+      justify-content: space-evenly;
+      flex-wrap: wrap;
+    }
+    .geral {
+      width: auto;
+    }
+    
+    fieldset{
+      width: 250px;
+      margin-bottom: 2%;
+    }
+      
+  </Style>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a style="text-decoration:none;" href="index.php">SaPHPatilha&nbsp; </a></li>
+          <li><a style="text-decoration:none;" href="produtos.php">Produtos&nbsp; </a></li>
+          <li><a style="text-decoration:none;" href="login.php">Login&nbsp; </a></li>
+          <li><a style="text-decoration:none;" href="cadastro.php">Cadastro</a></li>
+          <li><a style="text-decoration:none;" href="sobrenos.php">Sobre nós&nbsp; </a></li>
+        </ul>
+      </nav>
+    </header>
+    <fieldset class="geral">
+      <h1>Produtos</h1>
+        <section>   
+          <fieldset>
+            <div class="x-box-product">
+              <h3>
+                Sapatênis Tênis Masculino Polo Blu Casual Cinza
+              </h3>
+              <span class="best-price">
+                R$ 74,90
+              </span>
+            </div>
+          </fieldset>
+          <fieldset>
+            <div class="x-box-product">
+              <div class="product-name">
+                <h3>
+                  Sapatilha Couro Bottero Laço Detalhes à Laser Feminina
+                </h3>
+              </div>
+              <span class="best-price">
+                R$ 95,90
+              </span>
+            </div>
+          </fieldset>
+          <fieldset>
+            <div class="x-box-product">
+              <div class="product-name">
+                <h3>
+                  Tenis Botinha Star Cano Alto Bota All Conserve
+                </h3>
+              </div>
+              <span class="best-price">
+                R$ 249,99
+              </span>
+            </div>
+          </fieldset>
+          <fieldset>
+            <div class="x-box-product">
+              <div class="product-name">
+                <h3>
+                  Chinelo Top Marvel Logomania, Havaianas, Adulto Unissex
+                </h3>
+              </div>
+              <span class="best-price">
+                R$ 25,99
+              </span>
+            </div>
+          </fieldset>
+          <fieldset>
+            <div class="x-box-product">
+              <h3>
+                Sandália Feminina Salto Grosso Nobuck Nude E Preta
+              </h3>
+              <span class="best-price">
+                R$ 69,90
+              </span>
+            </div>
+          </fieldset>
+          <fieldset>
+            <div class="x-box-product">
+              <div class="product-name">
+                <h3>
+                  Bota Coturno Masculino Adventure Conforto
+                </h3>
+              </div>
+              <span class="best-price">
+                R$ 99,99
+              </span>
+            </div>
+          </fieldset>
+          <fieldset>
+            <div class="x-box-product">
+              <div class="product-name">
+                <h3>
+                  Salto Alto Brilhante Rosa Nobrand
+                </h3>
+              </div>
+              <span class="best-price">
+                R$ 110,50
+              </span>
+            </div>
+          </fieldset>
+          <fieldset>
+            <div class="x-box-product">
+              <div class="product-name">
+                <h3>
+                  Tênis Masculino Branco De Sola Baixa Vermelho Casual
+                </h3>
+              </div>
+              <span class="best-price">
+                R$ 104,99
+              </span>
+            </div>
+          </fieldset>
+        </section>
+    </fieldset>
+    
+    
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
